2be610171182d60c99c330ea4fe8f3eeddb48f51
Alex Musayev <alex.musayev@gmail.com>
2011-03-07 22:31:04 +0300

    Usings updated.

--- WordReplace/Program.cs
+++ WordReplace/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
-using System.Text;
 using Word = Microsoft.Office.Interop.Word;
 
 namespace WordReplace
